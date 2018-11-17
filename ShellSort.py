eede8a9b20e4324bff5743157cc81f8cb679b3de
FloTranquil <43479516+FloTranquil@users.noreply.github.com>
2018-11-17 13:57:19 +0300

    Update ShellSort.py

--- ShellSort.py
+++ ShellSort.py
@@ -1,11 +1,10 @@
 def shell(A):
-    t=int(len(A)/2)
-    while t>0:
+    t = int(len(A)/2)
+    while t > 0:
         for i in range (len(A)-t):
-            while i>=0 and A[i] > A[i+t]:
-                A[i], A[i+t] =  A[i+t], A[i]
-                i-=1
+            while i >= 0 and A [i] > A [i+t]:
+                A [i], A [i+t] = A [i+t], A [i]
+                i -= 1
         t = int(t/2)
-A=[2, 5, 1, 4, 10, 0, 9, 6, 3]
-shell(A)
-print (A)
+    return A
+A = [2, 5, 1, 4, 10, 0, 9, 6, 3]
