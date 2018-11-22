8b0e91c311f06629c5441c96e6115f5135537ce5
FloTranquil <43479516+FloTranquil@users.noreply.github.com>
2018-11-23 00:09:41 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,10 +1,12 @@
-#9. You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples. As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+#9. You need to write a function that reverses the words in a given string. 
+#A word can also fit an empty string. If this is not clear enough, here are some examples. 
+#As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
 
-x = input()
-def revers(x):
-    x = x.split()
-    x.reverse()
-    for i in x:
+words = input()
+def revers(words):
+    words = words.split()
+    words.reverse()
+    for i in words:
         print(i, end=' ')
     print()
-revers(x)
+revers(words)
