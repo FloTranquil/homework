c780b32af7501525a665d35cdb76b2f59a9fdf2d
FloTranquil <43479516+FloTranquil@users.noreply.github.com>
2018-12-11 17:46:32 +0300

    Add files via upload

---
+++ task_4.2.py
@@ -0,0 +1,10 @@
+def decorator(func):
+  def wrapper(*args, **kwargs):
+      result = func(*args, **kwargs)
+  return wrapper
+
+@decorator
+def full_name(first_name, last_name):
+  print ('My name is', first_name, last_name)
+
+full_name('Flo', 'Tranquil')
