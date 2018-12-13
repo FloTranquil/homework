099c7698896bc918c9d70217adb7b934f7bd87c5
FloTranquil <43479516+FloTranquil@users.noreply.github.com>
2018-12-13 22:19:28 +0300

    Update task_4.2.py

--- task_4.2.py
+++ task_4.2.py
@@ -1,10 +1,11 @@
 def decorator(func):
-  def wrapper(*args, **kwargs):
-      result = func(*args, **kwargs)
-  return wrapper
+    def wrapper(*args, **kwargs):
+        print ("Hello!", *args, **kwargs)
+        result = func(*args, **kwargs)
+    return wrapper
 
 @decorator
 def full_name(first_name, last_name):
-  print ('My name is', first_name, last_name)
+    print ('My name is', first_name, last_name)
 
 full_name('Flo', 'Tranquil')
